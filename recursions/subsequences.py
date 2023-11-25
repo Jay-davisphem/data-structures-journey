99e47e4d7c7fc72a544fa6c7a2808b6630e61c55
David Oluwafemi <davidoluwafemi178@gmail.com>
2023-11-25 07:47:00 +0100

    add array subsequences

---
+++ recursions/subsequences.py
@@ -0,0 +1,37 @@
+def print_sub_r(i: int, ds: list[object], arr: list[object]):
+    s = "\t" * i
+    if i == len(arr):
+        print(f"{s}r({i}, {ds}, {arr}) -><- ")
+        print(ds)
+        return
+    print(f"{s}r({i}, {ds}, {arr}) -> ")
+    ds.append(arr[i])
+    print_sub_r(i + 1, ds, arr)
+
+    ds.pop()
+    print(f"{s}r({i}, {ds}, {arr}) <- ")
+    print_sub_r(i + 1, ds, arr)
+
+
+def sub_sequences_r(arr: list[object]) -> list[list[object]]:
+    # Space complexity: O(2^n)
+    # Time complexity: O(2^n)
+    if len(arr) == 0:
+        return [[]]
+    else:
+        sub_seqs = sub_sequences_r(arr[1:])
+        return [[arr[0]] + sub_seq for sub_seq in sub_seqs] + sub_seqs
+
+
+def sub_sequences_r1(arr: list[object], i: int = 0) -> list[list[object]]:
+    # Space complexity: O(2^n)
+    # Time complexity: O(2^n)
+    if i == len(arr):
+        return [[]]
+    sub_sequences = sub_sequences_r1(arr, i + 1)
+    return [[arr[i]] + sub_sequence for sub_sequence in sub_sequences] + sub_sequences
+
+
+print(sub_sequences_r([3, 1, 2]))
+
+print_sub_r(0, [], [3, 1, 4])
